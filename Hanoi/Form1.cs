d516397241d6a23709a79ddfc8cd9e396cb0e705
Balytsky Igor <ibalyts@live.com>
2021-11-30 08:18:09 +0200

    Add project files.

---
+++ Hanoi/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Hanoi
+{
+    public static class Extantions
+    {
+        
+        public static int Pop(this List<int> list)
+        {
+            int item = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return item;
+        }
+
+    }
+    public partial class Form1 : Form
+    {
+        HanoiVisualisation hanoi;
+
+        public Form1()
+        {
+            InitializeComponent();
+            hanoi = new HanoiVisualisation(10, this);
+        }
+
+        void Hanoi(int n, int from, int to)
+        {
+            if (n == 0) return;
+
+            Hanoi(n - 1, from, 3 - from - to);
+            hanoi.SwapPlaces(from, to);
+            Hanoi(n - 1, 3 - from - to, to);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hanoi(0, 1);
+        }
+
+        private void Hanoi(int from, int to)
+        {
+            Hanoi(hanoi.Count, from, to);
+        }
+    }
+}
---
+++ Hanoi/HanoiVisualisation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Hanoi
+{
+    class HanoiVisualisation
+    {
+        Graphics g; 
+        List<List<int>> towers = new List<List<int>>(){
+            new List<int>(),
+            new List<int>(),
+            new List<int>()
+        };
+        Control parent;
+        public int Count { get; }
+
+        public HanoiVisualisation(int num, Control parent)
+        {
+            Count = num;
+            for (int i = Count; i > 0; i--)
+            {
+                towers[0].Add(i);
+            }
+            this.parent = parent;
+            g = parent.CreateGraphics();
+        }
+
+        public void SwapPlaces(int from, int to)
+        {
+            towers[to].Add(towers[from].Pop());
+            DrawTowers();
+            Thread.Sleep(50);
+        }
+
+
+        private void DrawTowers()
+        {
+            g.Clear(parent.BackColor);
+            Size size = new Size(parent.Width * 10 / towers.Count / 15, parent.Height * 80 / 100);
+            for (int i = 0; i < towers.Count; i++)
+            {
+                var tower = towers[i];
+                Point p = new Point(parent.Width / 3 * i + parent.Width / 12, parent.Height * 10 / 100);
+                int sum = towers.Sum(x => x.Count);
+                int h = size.Height / sum;
+                int d = h * 2 / 3;
+                for (int j = 0; j < tower.Count; j++)
+                {
+                    int disk = tower[j];
+                    int w = size.Width / sum * disk;
+                    Point p1 = new Point(p.X + size.Width / 2 - w / 2, size.Height - j * h);
+                    Point p2 = new Point(p.X + size.Width / 2 + w / 2, size.Height - j * h);
+                    g.DrawLine(new Pen(Color.FromArgb(disk * 255 / sum, 255 - disk * 125 / sum, 200), d), p1, p2);
+                }
+            }
+            parent.Update();
+        }
+
+
+    }
+}
